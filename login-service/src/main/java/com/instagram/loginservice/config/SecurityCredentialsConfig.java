e4e678e9bdb5a1c52c05831e4c55cbcfbbda637c
DuyTC <ferssken99@gmail.com>
2022-03-03 10:50:18 +0700

    Add SecurityCredentialsConfig

---
+++ login-service/src/main/java/com/instagram/loginservice/config/SecurityCredentialsConfig.java
@@ -0,0 +1,91 @@
+package com.instagram.loginservice.config;
+
+
+import com.instagram.loginservice.module.Role;
+import com.instagram.loginservice.service.JwtTokenProvider;
+import com.instagram.loginservice.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityCredentialsConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    private final JwtConfig jwtConfig;
+
+    private final JwtTokenProvider tokenProvider;
+
+    private final UserService userService;
+
+    @Value("${security.service.username}")
+    private String serviceUsername;
+
+    @Value("${security.service.password}")
+    private String servicePassword;
+
+    public SecurityCredentialsConfig(UserDetailsService userDetailsService, JwtConfig jwtConfig, JwtTokenProvider tokenProvider, UserService userService) {
+        this.userDetailsService = userDetailsService;
+        this.jwtConfig = jwtConfig;
+        this.tokenProvider = tokenProvider;
+        this.userService = userService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                .addFilterBefore(new JwtTokenAuthenticationFilter(serviceUsername, jwtConfig, tokenProvider, userService), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/signin").permitAll()
+                .antMatchers(HttpMethod.POST, "/users").anonymous()
+                .anyRequest().authenticated();
+    }
+
+    // Spring has UserDetailsService interface, which can be overriden to provide our implementation for fetching user from database (or any other source).
+    // The UserDetailsService object is used by the auth manager to load the user from database.
+    // In addition, we need to define the password encoder also. So, auth manager can compare and verify passwords.
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Configure in-memory authentication provider for service accounts for
+        // service inter-communication
+        auth.inMemoryAuthentication()
+                .withUser(serviceUsername)
+                .password(passwordEncoder().encode(servicePassword))
+                .roles(Role.SERVICE.getName());
+
+        // Configure DB authentication provider for user accounts
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean(name = "passwordEncoder")
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
